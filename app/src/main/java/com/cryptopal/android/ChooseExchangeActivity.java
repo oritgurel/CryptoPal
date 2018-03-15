5f1207e915193fc3332baa74567c5c1e9bdffae2
almog Shemesh <almogs5252@gmail.com>
2018-03-15 22:29:54 +0200

    add activity

--- app/src/main/java/com/cryptopal/android/ChooseExchangeActivity.java
+++ app/src/main/java/com/cryptopal/android/ChooseExchangeActivity.java
@@ -1,12 +1,37 @@
 package com.cryptopal.android;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
 
 /**
  * Created by user2 on 15/03/2018.
  */
 
 public class ChooseExchangeActivity extends AppCompatActivity {
+
+    String [] exchange = new String[]{
+            "one",
+            "two",
+            "tree",
+            "for",
+            "five",
+            "six",
+            "seven",
+
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_exchange);
+
+        ListView exchange = (ListView) findViewById(R.id.exchange);
+
+        ArrayAdapter adapter = new ArrayAdapter()
+    }
 
 
 }
---
+++ app/src/main/java/com/cryptopal/android/ChooseExchangeAdapter.java
@@ -0,0 +1,28 @@
+package com.cryptopal.android;
+
+import android.app.Fragment;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Adapter;
+import android.widget.SimpleAdapter;
+
+/**
+ * Created by Test on 15/03/2018.
+ */
+
+public class ChooseExchangeAdapter extends ListActivity  {
+
+    SimpleAdapter mAdapter;
+
+    protected  void onCreate (Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+    }
+
+
+
+
+
+
+
+}
