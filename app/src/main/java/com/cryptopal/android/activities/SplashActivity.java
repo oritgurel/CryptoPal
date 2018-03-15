88f8aee1579333d699085869fcbbdf18333e00d1
Daniel Szasz <danysz@gmail.com>
2018-03-15 23:16:45 +0200

    sign in with basic graphics

--- app/src/main/java/com/cryptopal/android/activities/SplashActivity.java
+++ app/src/main/java/com/cryptopal/android/activities/SplashActivity.java
@@ -6,14 +6,25 @@ import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.cryptopal.android.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    private static final String TAG = "SplashActivity";
+
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
+    private static final int RC_SIGN_IN = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +43,59 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent startActivityIntent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(startActivityIntent);
-                SplashActivity.this.finish();
+                Intent intentNext;
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                if (auth.getCurrentUser() != null) {
+                    // already signed in
+                    intentNext = new Intent( SplashActivity.this, ChatActivity.class);
+                    startActivity(intentNext);
+                    SplashActivity.this.finish();
+                } else {
+                    // not signed in
+//                    intentNext = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivityForResult(
+                            // Get an instance of AuthUI based on the default app
+                            AuthUI.getInstance().createSignInIntentBuilder().
+                                    setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build())).
+                                    build(),
+                            RC_SIGN_IN);
+                }
+
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Intent intentNext;
+
+                intentNext = new Intent( SplashActivity.this, ChatActivity.class);
+                startActivity(intentNext);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+//                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+//                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+//                showSnackbar(R.string.unknown_error);
+                Log.e(TAG, "Sign-in error: ", response.getError());
+            }
+        }
     }
 }
