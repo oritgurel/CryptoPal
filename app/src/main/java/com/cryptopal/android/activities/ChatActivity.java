9d7ebfb61d851f77700b6f6508c186eb1329cddd
Daniel Szasz <danysz@gmail.com>
2018-03-16 00:07:04 +0200

    start adding stuff for chat

--- app/src/main/java/com/cryptopal/android/activities/ChatActivity.java
+++ app/src/main/java/com/cryptopal/android/activities/ChatActivity.java
@@ -1,13 +1,28 @@
 package com.cryptopal.android.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-/**
- * Created by user2 on 15/03/2018.
- */
+import com.cryptopal.android.R;
 
 public class ChatActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_chat, menu);
+        return true;
+    }
 }
