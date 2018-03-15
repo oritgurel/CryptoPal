c4510ece0ff948405d6fc49d683bf96aa3c5e1fb
almog Shemesh <almogs5252@gmail.com>
2018-03-15 23:04:36 +0200

    exchange

--- app/src/main/java/com/cryptopal/android/ChooseExchangeAdapter.java
+++ app/src/main/java/com/cryptopal/android/ChooseExchangeAdapter.java
@@ -4,7 +4,11 @@ import android.app.Fragment;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 /**
@@ -13,16 +17,30 @@ import android.widget.SimpleAdapter;
 
 public class ChooseExchangeAdapter extends ListActivity  {
 
-    SimpleAdapter mAdapter;
+    String [] exchange = new String[]{
+            "one",
+            "two",
+            "tree",
+            "for",
+            "five",
+            "sixs",
+            "seven",
+    };
 
     protected  void onCreate (Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_choose_exchange);
+
+        ListView exchange = (ListView)findViewById(R.id.exchange);
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout .activity_list_item);
+        exchange.setAdapter(adapter);
+
+        exchange.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int postion, long Id){
+
+            }
+
+        });
     }
-
-
-
-
-
-
 
 }
