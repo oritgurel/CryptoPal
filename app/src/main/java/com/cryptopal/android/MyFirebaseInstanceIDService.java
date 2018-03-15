03068ec79f807446efcdf387c7da298a8fa733b3
Oritmalki <oritgurel@gmail.com>
2018-03-15 18:35:41 +0200

    test

---
+++ app/src/main/java/com/cryptopal/android/MyFirebaseInstanceIDService.java
@@ -0,0 +1,38 @@
+package com.cryptopal.android;
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by user2 on 15/03/2018.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FirebaseIDService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate");
+
+    }
+
+    //this method will be called
+    //when the token is generated
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        //now we will have the token
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        //for now we are displaying the token in the log
+        //copy it as this method is called only when the new token is generated
+        //and usually new token is only generated when the app is reinstalled or the data is cleared
+        Log.d(TAG, token);
+    }
+}
